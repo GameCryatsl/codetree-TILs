0bd13fb3f60e89f4540cf63122a9649ddb6d06e5
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-01-21 00:53:25 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240121/중앙값 구하기/find-the-median.cpp
+++ 240121/중앙값 구하기/find-the-median.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if ((a >= b && b >= c) || (c >= b && b >= a)) {
+    if ((a > b && b > c) || (c > b && b > a)) {
         cout << b;
     }
-    else if ((b >= a && a >= c) || (c >= a && a >= b)) {
+    else if ((b > a && a > c) || (c > a && a > b)) {
         cout << a;
     }
     else cout << c;
