99617d9ae5535c74f43b10e896c403b33ccdb20d
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-01-21 00:34:59 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240121/윤년인가/is-leap-year.cpp
+++ 240121/윤년인가/is-leap-year.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int y;
     cin >> y;
-    if (y % 4 == 0 && !(y % 100 == 0)) {
+    if (y % 4 == 0 && !(y % 100 == 0) || y % 400 == 0) {
         cout << "true";
     }
     else cout << "false";
