6361a60da91b016d47f649ed54e32ad60ec86371
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-01-21 00:21:27 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a1, a2, b1, b2;
+    cin >> a1 >> a2 >> b1 >> b2;
+    if (a1 > b1) {
+        cout << "A";
+    }
+    else if (b1 > a1) {
+        cout << "B";
+    }
+    else if (a2 > b2) {
+        cout << "A";
+    }
+    else if (b2 < a2) {
+        cout << "B";
+    }
+    return 0;
+}
