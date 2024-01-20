575ec5f74330911e8fb7321a8ffd1ed998fa48d4
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-01-21 00:39:48 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240121/일 수 구하기/number-of-days-in-month.cpp
+++ 240121/일 수 구하기/number-of-days-in-month.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    if(n % 2 == 0 && !(n == 2) && n < 8) {
+    if(n % 2 == 0 && !(n == 2) && n <= 8) {
         cout << "30";
     }
     else if (n == 2) {
         cout << "28";
     }
-    else if (n % 2 == 1 && !(n == 2) && n < 8) {
+    else if (n % 2 == 1 && !(n == 2) && n <= 8) {
         cout << "31";
     }
     else if (n % 2 == 1) {
