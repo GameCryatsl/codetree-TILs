dd5aa80df582e993fc758d7add2832cb832cc756
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-02-05 23:50:01 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240205/문자열 출력하기/print-string.cpp
+++ 240205/문자열 출력하기/print-string.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < n; i++) {
         cout << "LeebrosCode" << endl;
     }
     return 0;
