da342b2d798465a0c23056cf9dfa97ffab93ef1d
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-02-24 17:58:45 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240224/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 240224/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(int i = 1; i <= 40; i++) {
+    for(int i = 1; i <= n; i++) {
         if((i % 2 == 0 && i % 4 != 0) || (i % 7 < 4) || ((i / 8)% 2 == 0)) {
             continue;
         }
