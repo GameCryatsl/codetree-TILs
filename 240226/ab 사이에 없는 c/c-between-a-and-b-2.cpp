5ab0250ac8013dd5d848973ef5b94208c17765c4
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-02-26 16:00:45 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, chk = 0;
+    cin >> a >> b >> c;
+    for(int i = a; i <= b; i++) {
+        if(i % c == 0) {
+            chk = 1;
+            break;
+        }
+    }
+    if(chk) {
+        cout << "NO";
+    }
+    else cout << "YES";
+    return 0;
+}
