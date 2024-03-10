82292efbae7164e86f7e4c8ea794380014d0a185
GameCryatsl <117077704+GameCryatsl@users.noreply.github.com>
2024-03-10 23:40:35 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || i == n - 1) {
+            for(int j = 0; j < n; j++) {
+                cout << "* ";
+            }
+        }
+        else {
+            for(int j = 0; j < i; j++) {
+                cout << "* ";
+            }
+            for(int j = 0; j < n - i - 1; j++) {
+                cout << "  ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
